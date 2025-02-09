0145b4948805890b5fecfb6da800b29165357d72
22omarayman <omarayman2004@gmail.com>
2025-02-09 20:02:05 +0200

    ui and setup done

---
+++ src/app/modules/code-analyzer/code-analyzer/code-analyzer.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from "@angular/common";
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-code-analyzer',
+  imports: [CommonModule],
+  templateUrl: './code-analyzer.component.html',
+  styleUrl: './code-analyzer.component.scss'
+})
+export class CodeAnalyzerComponent {
+
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{html,ts}"
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
